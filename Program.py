f69110c57d2114fae9eaba4357bfbcaf2144e16c
gilad hadad <giladhadad11@gmail.com>
2023-09-06 11:52:10 +0300

    Adding to the file Program.py the function ‘assembly_of_number’

--- Program.py
+++ Program.py
@@ -1,8 +1,19 @@
+def assembly_of_number(number: int) -> int:
+    multipication = 1
+    for i in range(1,number):
+        multipication *= i
+    return multipication
+
+
 def main():
     sum = 0
     for i in range(1,100):
         sum += i
-    return sum
+    
+    print(assembly_of_number(5))
+    print(assembly_of_number(6))
+    print(assembly_of_number(7))
+    print(assembly_of_number(8))
 
 if __name__ == "__main__":
     main()
