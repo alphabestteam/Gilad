4f15d3e99059fe6545730d9b56ddb8db2cecf94b
gilad hadad <giladhadad11@gmail.com>
2023-09-06 13:24:25 +0300

    Adding to the file the function prime number

--- Program.py
+++ Program.py
@@ -1,8 +1,26 @@
+def prime_number(number: int) -> str:
+    flag = True
+    for i in range(2,number):
+        if number % i == 0:
+            flag = False
+    if flag:
+        print(f"{number} is a prime number")
+    else:
+        print(f"{number} is not a prime number")
+
+
 def main():
     sum = 0
     for i in range(1,100):
         sum += i
-    return sum
+    # return sum
+    
+    prime_number(5)
+    prime_number(6)
+    prime_number(7)
+    prime_number(14)
+    prime_number(152)
+    prime_number(60693)
 
 if __name__ == "__main__":
     main()
