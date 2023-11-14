d4fc6d7a6a626af506467eb7f634e31bc5b316d5
gilad hadad <giladhadad11@gmail.com>
2023-11-14 19:32:32 +0200

    adding functionality

--- pdf12.js
+++ pdf12.js
@@ -34,7 +34,7 @@ const secondStudent = {
 }
 const thirdStudent = {
     name: "Yonatan",
-    age: 30,
+    age: 16,
     grades: [95, 96, 97, 98, 99],
     average: function () {
         let sum = this.grades.reduce(function (a, b) {
@@ -89,3 +89,29 @@ console.log(secondStudent);
 console.log(thirdStudent);
 console.log(forthStudent);
 console.log(fifthStudent);
+
+const studentsArray = [firstStudent, secondStudent, thirdStudent, forthStudent, fifthStudent];
+
+for (let i = 0; i < studentsArray.length; i++) {
+    console.log(`The index of the student ${studentsArray[i].name} in students array is: ${i}`);
+}
+
+
+for (let i = 0; i < studentsArray.length; i++) {
+    console.log(`name: ${studentsArray[i].name}`);
+    console.log(`age: ${studentsArray[i].age}`);
+    console.log(`grades: ${studentsArray[i].grades}`);
+    console.log(`average: ${studentsArray[i].average()}`);
+    console.log(`\n`);
+}
+
+
+function checkAdult(student) {
+    return student.age >= 18;
+}
+
+
+const adultsArray = studentsArray.filter(checkAdult);
+console.log(adultsArray);
+
+
